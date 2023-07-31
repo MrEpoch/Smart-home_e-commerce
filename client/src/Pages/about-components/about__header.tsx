fac951fef0d8c3900621b4e3a47aaf00f29e85d1
MrEpoch <alexandrstencuk@seznam.cz>
2023-07-31 21:42:06 +0200

    About page is completed to some degree, now starting on contact page

--- client/src/Pages/about-components/about__header.tsx
+++ client/src/Pages/about-components/about__header.tsx
@@ -1,6 +1,27 @@
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import House from "../../assets/house-design.jpg";
 import "./about__header.css";
+import Lightning from "./about_svgs/lightning-bolt.svg";
+import InfinitySvg from "./about_svgs/infinity.svg";
+import LightBulbOn from "./about_svgs/lightbulb-on-outline.svg";
+
+export const goalCards = [
+    {
+        title: "Spread technology",
+        text: "Making your home more advanced and secure.",
+        image: Lightning
+    },
+    {
+        title: "Show maximum",
+        text: "Showing maximum potential of current technology.",
+        image: InfinitySvg
+    },
+    {
+        title: "Make it easy",
+        text: "Making life easire for you and your family.",
+        image: LightBulbOn
+    }
+]
 
 export default function About__header() {
     return (
@@ -12,14 +33,27 @@ export default function About__header() {
                     <br/>
                     We are technological company who wants to give you and your home to be most advanced and secure you can get.
                     <br/>
-                    We are spreading our work all over the world with most famous engineers and designers to optimize and enhance products
+                    Our goal is to spread our work all over the world with most famous engineers and designers to optimize and enhance products
                     to be of highest quality and use for your home.
                 </p>
             </div>
             <img src={House} alt="House design" className="
                 about__header__image
                 w-100  flex 
-        "/>
+            "/>
+            <div className="about__header__goal-cards">
+                {goalCards.map((card, index) => (
+                    <Card key={index} className="about__header__goal-card">
+                        <Card.Img variant="top" src={card.image} alt="Card icon" className="about__header__goal-card__icon"/>
+                        <Card.Body>
+                            <Card.Title>{card.title}</Card.Title>
+                            <Card.Text>
+                                {card.text}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
         </Container>
     )
 }
---
+++ client/src/Pages/contact-components/contact-info.tsx
@@ -0,0 +1,8 @@
+
+export default function ContactInfo() {
+    return (
+        <div className="Contact__info">
+            <h1 className="Contact__info__title">Contact us</h1>
+        </div>
+    )
+}
